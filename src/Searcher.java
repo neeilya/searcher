4802079a76a52383413637be6a988646f956f172
neeilya <neeilya.94@gmail.com>
2014-12-05 21:51:26 +0600

    Searcher class added

---
+++ src/Searcher.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Ilya
+ *
+ */
+public class Searcher {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
